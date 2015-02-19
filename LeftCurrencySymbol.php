de430a63155a5c92385440778a74d7f546305d75
George Palmer <george@honeystone.com>
2015-02-19 09:43:29 +0000

    Added LeftCurrencySymbol module

---
+++ LeftCurrencySymbol.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file belongs to the Thelia Left Currency Symbol module
+ *
+ * @copyright Copyright (c) 2015, Honeystone Consulting Ltd.
+ */
+
+namespace LeftCurrencySymbol;
+
+use Thelia\Module\BaseModule;
+
+/**
+ * LeftCurrencySymbol class
+ *
+ * @author George Palmer <george@honeystone.com>
+ */
+class LeftCurrencySymbol extends BaseModule {
+
+}
